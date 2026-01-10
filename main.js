10512e2133791552e43c8b9db3af5134408e985d
minspark.dev <minspark.dev@gmail.com>
2026-01-10 12:37:44 +0000

    Update project files: index.html, main.js, style.css and remove README.md

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-// Add JS here
+
+class SimpleGreeting extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    const wrapper = document.createElement('span');
+    wrapper.setAttribute('class', 'wrapper');
+    const text = document.createElement('p');
+    text.textContent = `Hello, ${this.getAttribute('name') || 'World'}!`;
+    const style = document.createElement('style');
+    style.textContent = `
+      .wrapper {
+        padding: 15px;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        background-color: var(--card-bg-color);
+        box-shadow: var(--shadow);
+      }
+    `;
+    shadow.appendChild(style);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(text);
+  }
+}
+
+customElements.define('simple-greeting', SimpleGreeting);
